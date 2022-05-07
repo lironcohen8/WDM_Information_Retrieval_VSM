94e1b2b062d35151bd7a8129b136ffae32353f2b
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-05-07 21:19:43 +0300

    finished part A

--- vsm_ir.py
+++ vsm_ir.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
+from sympy import re
 nltk.download("stopwords")
 from nltk.stem import PorterStemmer
 ps = PorterStemmer()
@@ -23,20 +24,20 @@ def create_index(dir_path):
         records = root.findall("RECORD")
         for record in records:
             max_freq = 0
-            words_cnt = 0
             record_num = record.find("RECORDNUM").text.strip()
             records_dict[record_num] = {"words_cnt" : 0, "words" : {}}
             record_words = get_words_from_record(record)
             for word in record_words:
-                clean_word, validationValue = update_word_dict(word, record_num)
-                words_cnt += validationValue
-                if validationValue > 0:
+                # stemming, removing pactuations and coverting to lower case
+                clean_word = ps.stem(word.translate(str.maketrans('', '', string.punctuation)).lower())  
+                if clean_word not in set(stopwords.words('english')):
+                    update_words_dict(clean_word, record_num)               
+                    records_dict[record_num]["words_cnt"] += 1
                     max_freq = max(max_freq, words_dict[clean_word]["docs"][record_num])
             calc_tf_values(record_num, max_freq)
-            records_dict[record_num]["word_cnt"] = words_cnt
             print(record_num)
     calc_idf_values()
-    save_words_dict_to_json()
+    save_index_dict_to_json()
 
 def get_words_from_record(record):
     title_words = record.find("TITLE").text.strip().split()
@@ -47,24 +48,20 @@ def get_words_from_record(record):
     record_words = title_words + abstract_words + extract_words
     return record_words           
 
-def update_word_dict(word, record_num):
-    validationValue = 0
-    # stemming, removing pactuations and coverting to lower case
-    clean_word = ps.stem(word.translate(str.maketrans('', '', string.punctuation)).lower())  
-    if clean_word not in set(stopwords.words('english')):
-        validationValue = 1
-        if clean_word not in words_dict.keys():
-            words_dict[clean_word] = {"docs" : {}, "idf": 0}
-        if record_num not in words_dict[clean_word]["docs"].keys():
-            words_dict[clean_word]["docs"][record_num] = 0
-        words_dict[clean_word]["docs"][record_num] += 1
-    return clean_word, validationValue
-        
+def update_words_dict(clean_word, record_num):
+    if clean_word not in words_dict.keys():
+        words_dict[clean_word] = {"docs" : {}, "idf": 0}
+    if record_num not in words_dict[clean_word]["docs"].keys():
+        words_dict[clean_word]["docs"][record_num] = 0
+    words_dict[clean_word]["docs"][record_num] += 1
+
 def calc_tf_values(record_num, max_freq):
     for word in words_dict.keys():
         # normalize value to get tf for every word in every record
         if record_num in words_dict[word]["docs"].keys():
-            words_dict[word]["docs"][record_num] = words_dict[word]["docs"][record_num] / max_freq
+            tf = words_dict[word]["docs"][record_num] / max_freq
+            words_dict[word]["docs"][record_num] = tf
+            records_dict[record_num]["words"][word] = tf
 
 def calc_idf_values():
     D = len(words_dict.keys())
@@ -72,7 +69,7 @@ def calc_idf_values():
         df = len(words_dict[word]["docs"])
         words_dict[word]["idf"] = math.log2(D / df) 
 
-def save_words_dict_to_json():
+def save_index_dict_to_json():
     index_dicts = {"words_dict" : words_dict, "records_dict" : records_dict}
     with open(JSON_FILE_NAME, 'w') as f:
         json.dump(index_dicts, f, indent=4)
