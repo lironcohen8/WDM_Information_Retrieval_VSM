654824a14c5d982d7033d7fbaead646d1da79fc0
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-05-07 22:12:53 +0300

    Update vsm_ir.py

--- vsm_ir.py
+++ vsm_ir.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 import string
 import math
@@ -13,6 +14,7 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 JSON_FILE_NAME = "vsm_inverted_index.json"
+QUERY_RESULT_FILE_NAME = "ranked_query_docs.txt"
 records_dict = {}
 words_dict = {}
 
@@ -67,16 +69,87 @@ def calc_idf_values():
     D = len(words_dict.keys())
     for word in words_dict.keys():
         df = len(words_dict[word]["docs"])
-        words_dict[word]["idf"] = math.log2(D / df) 
+        words_dict[word]["idf"] = math.log2(D / df)
+
+def calc_weight_values():
+    for word in words_dict.keys():
+        for record_num in words_dict[word]["docs"].keys():
+            idf = words_dict[word]["idf"]
+            words_dict[word]["docs"][record_num] *= idf
+            records_dict[record_num]["words"][word] *= idf
 
 def save_index_dict_to_json():
     index_dicts = {"words_dict" : words_dict, "records_dict" : records_dict}
     with open(JSON_FILE_NAME, 'w') as f:
         json.dump(index_dicts, f, indent=4)
 
+
+
+def ask_question(ranking, index_path, query):
+    words_dict, records_dict = load_index_dict_from_json(index_path)
+    query_dict = parse_query(query)
+    if ranking == "tfidf":
+        sorted_records = calc_tfidf_grades(query_dict)
+    else:
+        sorted_records = calc_bm25_grades(query_dict)
+    save_query_result_to_txt(sorted_records)
+
+def parse_query(query):
+    query_dict = {"words_cnt" : 0, "words" : {}}
+    query_words = query.strip().split()
+    for word in query_words:
+        # stemming, removing pactuations and coverting to lower case
+        clean_word = ps.stem(word.translate(str.maketrans('', '', string.punctuation)).lower())  
+        if clean_word not in set(stopwords.words('english')):
+            if clean_word not in query_dict["words"].keys():
+                query_dict["words"][clean_word] = 0
+            query_dict["words"][clean_word] += 1
+            query_dict["words_cnt"] += 1
+            max_freq = max(max_freq, query_dict["words"][clean_word])
+
+    # Calculate idf
+    D = query_dict["words_cnt"]
+    idf = math.log2(D)
+
+    # Calculate tf
+    for word in query_dict["words"].keys():
+        query_dict["words"][word] = query_dict["words"][word] * idf / max_freq    
+    return query_dict
+
+def load_index_dict_from_json(index_path):
+    with open(index_path) as f:
+        index_dict = json.load(f, indent=4)
+    return index_dict[words_dict], index_dict[records_dict]
+
+def calc_tfidf_grades(query_dict):
+    nomin = 0
+    denom_query = 0
+    denom_record = 0
+    relevant_records = {}
+
+    for record_num in records_dict.keys():
+        common_words = query_dict["words"].keys() & records_dict[record_num]["words"].keys()
+        if len(common_words > 0):
+            for word in common_words:
+                query_weight = query_dict["words"][word]
+                record_weight = records_dict[record_num]["words"][word]
+                nomin += (query_weight * record_weight)
+                denom_query += (query_weight * query_weight)
+                denom_record += (record_weight * record_weight)
+            cos_similarity = nomin / (math.sqrt(denom_query) * math.sqrt(denom_record))
+            relevant_records[record_num] = cos_similarity
+    
+    sorted_records = sorted(relevant_records.items(), key=lambda x: x[1], reverse=True)
+    return sorted_records
+
+
+
+def save_query_result_to_txt(sorted_records):
+    with open(QUERY_RESULT_FILE_NAME, 'w') as f:
+        f.write(os.linesep).join(sorted_records)
+
 if __name__ == '__main__':
     if sys.argv[1] == "create_index":
         create_index(sys.argv[2])
     elif sys.argv[1] == "query":
-        #ask_question(sys.argv[2])
-        pass
+        ask_question(sys.argv[2], sys.argv[3], sys.argv[4])
