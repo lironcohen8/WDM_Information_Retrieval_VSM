acaecf54c8ad1fa88fa61026c1ad46eabea0f01e
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-05-07 18:44:44 +0300

    more work

--- vsm_ir.py
+++ vsm_ir.py
@@ -1,36 +1,74 @@
-from contextlib import AbstractAsyncContextManager
 import sys
+import string
+import math
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-def create_index_dict(dir_path):
-    records_dict = {}
+records_dict = {}
+words_dict = {}
+
+def create_index(dir_path):
     files = Path(dir_path).glob('*.xml')
     for file in files:
         doc = ET.parse(file)
         root = doc.getroot()
         records = root.findall("RECORD")
         for record in records:
+            max_freq = 0
+            words_cntr = 0
             record_num = record.find("RECORDNUM").text.strip()
             records_dict[record_num] = {}
-            title_words = record.find("TITLE").text.strip().split()
-            abstract_words = record.find("ABSTRACT")
-            abstract_words = abstract_words.text.strip().split() if abstract_words != None else []
-            extract_words = record.find("EXTRACT")
-            extract_words = extract_words.text.strip().split() if extract_words != None else []          
-            record_words = title_words + abstract_words + extract_words
+            record_words = get_words_from_record(record)
             for word in record_words:
-                if (word not in records_dict[record_num].keys()):
-                    records_dict[record_num][word] = 0
-                records_dict[record_num][word] += 1
-    pass
+                clean_word = update_word_dict(word, record_num)
+                max_freq = max(max_freq, words_dict[clean_word]["docs"][record_num])
+            calc_tf_values(record_num, max_freq)
             
-            
+            records_dict[record_num] = words_cntr
+            print(record_num)
+    calc_idf_values()
 
+def get_words_from_record(record):
+    title_words = record.find("TITLE").text.strip().split()
+    abstract_words = record.find("ABSTRACT")
+    abstract_words = abstract_words.text.strip().split() if abstract_words != None else []
+    extract_words = record.find("EXTRACT")
+    extract_words = extract_words.text.strip().split() if extract_words != None else []          
+    record_words = title_words + abstract_words + extract_words
+    return record_words           
+
+def update_word_dict(word, record_num):
+    # stemming, removing pactuations and coverting to lower case
+    clean_word = ps.stem(word.translate(str.maketrans('', '', string.punctuation)).lower())  
+    if clean_word not in set(stopwords.words('english')):
+        words_cntr += 1
+        if clean_word not in words_dict.keys():
+            words_dict[clean_word] = {"docs" : {}, "idf": 0}
+        if record_num not in words_dict[clean_word]["docs"].keys():
+            words_dict[clean_word]["docs"][record_num] = 0
+        words_dict[clean_word]["docs"][record_num] += 1
+    return clean_word
+        
+def calc_tf_values(record_num, max_freq):
+    for word in words_dict.keys():
+        # normalize value to get tf for every word in every record
+        if record_num in words_dict[word]["docs"].keys():
+            words_dict[word]["docs"][record_num] = words_dict[word]["docs"][record_num] / max_freq
+
+def calc_idf_values():
+    D = len(words_dict.keys())
+    for word in words_dict.keys():
+        df = len(words_dict[word]["docs"])
+        words_dict[word]["idf"] = math.log2(D / df) 
 
 if __name__ == '__main__':
     if sys.argv[1] == "create_index":
-        create_index_dict(sys.argv[2])
+        create_index(sys.argv[2])
     elif sys.argv[1] == "query":
         #ask_question(sys.argv[2])
         pass
