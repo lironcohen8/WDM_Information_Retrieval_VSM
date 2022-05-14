4d10192d930ff2e7aaa4b8f898ee0e7fc6b6806a
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2022-05-14 21:48:39 +0300

    added tester

--- vsm_ir.py
+++ vsm_ir.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 JSON_FILE_NAME = "vsm_inverted_index.json"
 QUERY_RESULT_FILE_NAME = "ranked_query_docs.txt"
+SCORELIMIT = 0.05
 BM25_K = 1.5
 BM25_B = 0.75
 records_dict = {}
@@ -26,7 +27,7 @@ def create_index(dir_path):
         records = root.findall("RECORD")
         for record in records:
             max_freq = 0
-            record_num = record.find("RECORDNUM").text.strip()
+            record_num = record.find("RECORDNUM").text.strip().lstrip('0')
             records_dict[record_num] = {"words_cnt" : 0, "words" : {}}
             record_words = get_words_from_record(record)
             for word in record_words:
@@ -35,7 +36,7 @@ def create_index(dir_path):
                 if clean_word not in set(stopwords.words('english')):
                     update_words_dict(clean_word, record_num)               
                     records_dict[record_num]["words_cnt"] += 1
-                    max_freq = max(max_freq, words_dict[clean_word]["docs"][record_num])
+                    max_freq = max(max_freq, words_dict[clean_word]["docs"][record_num]["words_cnt"])
             calc_tf_values(record_num, max_freq)
             print(record_num)
     calc_idf_values()
@@ -46,27 +47,29 @@ def get_words_from_record(record):
     abstract_words = record.find("ABSTRACT")
     abstract_words = abstract_words.text.strip().split() if abstract_words != None else []
     extract_words = record.find("EXTRACT")
-    extract_words = extract_words.text.strip().split() if extract_words != None else []          
-    record_words = title_words + abstract_words + extract_words
+    extract_words = extract_words.text.strip().split() if extract_words != None else []  
+    topic_words = record.find("TOPIC")
+    topic_words = extract_words.text.strip().split() if topic_words != None else []          
+    record_words = title_words + abstract_words + extract_words + topic_words
     return record_words           
 
 def update_words_dict(clean_word, record_num):
     if clean_word not in words_dict.keys():
         words_dict[clean_word] = {"docs" : {}, "idf": 0}
     if record_num not in words_dict[clean_word]["docs"].keys():
-        words_dict[clean_word]["docs"][record_num] = 0
-    words_dict[clean_word]["docs"][record_num] += 1
+        words_dict[clean_word]["docs"][record_num] = {"words_cnt": 0, "tf": 0}
+    words_dict[clean_word]["docs"][record_num]["words_cnt"] += 1
 
 def calc_tf_values(record_num, max_freq):
     for word in words_dict.keys():
         # normalize value to get tf for every word in every record
         if record_num in words_dict[word]["docs"].keys():
-            tf = words_dict[word]["docs"][record_num] / max_freq
-            words_dict[word]["docs"][record_num] = tf
-            records_dict[record_num]["words"][word] = tf
+            tf = words_dict[word]["docs"][record_num]["words_cnt"] / max_freq
+            words_dict[word]["docs"][record_num]["tf"] = tf
+            records_dict[record_num]["words"][word] = {"tf-idf": tf} # temporary tf in tf-idf slot
 
 def calc_idf_values():
-    D = len(words_dict.keys())
+    D = len(records_dict.keys())
     for word in words_dict.keys():
         df = len(words_dict[word]["docs"])
         words_dict[word]["idf"] = math.log2(D / df)
@@ -75,7 +78,7 @@ def calc_weight_values():
     for word in words_dict.keys():
         for record_num in words_dict[word]["docs"].keys():
             idf = words_dict[word]["idf"]
-            records_dict[record_num]["words"][word] *= idf
+            records_dict[record_num]["words"][word]["tf-idf"] *= idf # updates tf to tf-idf
 
 def save_index_dict_to_json():
     index_dicts = {"words_dict" : words_dict, "records_dict" : records_dict}
@@ -132,14 +135,18 @@ def calc_tfidf_grades(query_dict):
         if len(common_words) > 0:
             for word in common_words:
                 query_weight = query_dict["words"][word]
-                record_weight = records_dict[record_num]["words"][word]
+                record_weight = records_dict[record_num]["words"][word]["tf-idf"]
                 nomin += (query_weight * record_weight)
-                denom_query += (query_weight * query_weight)
-                denom_record += (record_weight * record_weight)
-            cos_similarity = nomin / (math.sqrt(denom_query) * math.sqrt(denom_record))
-            relevant_records[record_num] = cos_similarity
-    
-    sorted_records = sorted(relevant_records.items(), key=lambda x: x[1], reverse=True)
+        for word in query_dict["words"].keys():
+            query_weight = query_dict["words"][word]
+            denom_query += (query_weight * query_weight)
+        for word in records_dict[record_num]["words"].keys():
+            record_weight = records_dict[record_num]["words"][word]["tf-idf"]
+            denom_record += (record_weight * record_weight)
+        cos_similarity = nomin / (math.sqrt(denom_query) * math.sqrt(denom_record))
+        relevant_records[record_num] = cos_similarity
+    relevant_records_list = [record for record in relevant_records.items() if record[1] > SCORELIMIT]
+    sorted_records = sorted(relevant_records_list, key=lambda x: x[1], reverse=True)
     return sorted_records
 
 def calc_bm25_grades(query_dict):
@@ -163,7 +170,7 @@ def calc_bm25_grade_for_record(D, avgdl, N, query_dict, record_num):
         for word in common_words:
             n = len(words_dict[word]["docs"].keys())
             idf = math.log(1 + ((N - n + 0.5) / (n + 0.5)))
-            tf = words_dict[word]["docs"][record_num]
+            tf = words_dict[word]["docs"][record_num]["tf"]
             nomin = idf * tf * (BM25_K + 1)
             right_denom = BM25_K * (1 - BM25_B + (BM25_B * D / avgdl))
             bm25_grade += (nomin / tf + right_denom)
